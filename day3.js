11311ac49d8d3858b9ee0f7de7171e337519eaae
Afrin jahan Rimi <116110521+1-2op@users.noreply.github.com>
2024-11-03 20:59:53 +0600

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,23 @@
+//let score= true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber =Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=TRUE; 0=FALSE
+
+let someNumber =42
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console .log( typeof stringNumber);
+
