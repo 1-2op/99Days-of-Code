f44555690cc04565a3c7fa7554ac4a360134167d
Afrin jahan Rimi <116110521+1-2op@users.noreply.github.com>
2024-11-05 22:27:30 +0600

    Add files via upload

---
+++ day5.js
@@ -0,0 +1,33 @@
+//let gameCounter = 100
+//gameCounter++;
+//console.log(gameCounter);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+//prefix / postfix
+
+//comparison
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+//console.log("3" > 1);
+//console.log("03" > 1);
+
+console.log(null  > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);  
+
+//===
+
+console.log("3" === 3);
