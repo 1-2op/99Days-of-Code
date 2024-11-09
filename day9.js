193beeacf7d6fdb038f5edbbbb3cc230c23c24fe
Afrin jahan Rimi <116110521+1-2op@users.noreply.github.com>
2024-11-09 23:30:29 +0600

    Add files via upload

---
+++ day9.js
@@ -0,0 +1,15 @@
+const newStringOne = "  afrin  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://afrin.com/afrin%20choudhary"
+
+console.log(url.replace('%20','-'))
+
+//console.log(url.includes('com'))
+
+console.log(url.includes('tia'))
+
+const gameName = new String('afrin')
+
+console.log(gameName.split('-'));
