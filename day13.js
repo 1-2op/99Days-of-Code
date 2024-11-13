53ca84b860f58815950e5a567cfcd7c06b1470fe
Afrin jahan Rimi <116110521+1-2op@users.noreply.github.com>
2024-11-13 23:57:43 +0600

    Add files via upload

---
+++ day13.js
@@ -0,0 +1,27 @@
+let newDate = new Date()
+//console.log(newDate);
+//console.log(newDate.getMonth() + 1);
+//console.log(newDate.getDay);
+
+newDate.toLocaleDateString('default',  { 
+    weekday: "long"
+})
+
+// array**
+
+const myArr = [0, 1, 3, 5, 7]
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+
+//array methods
+
+//myArr.push(6)
+//myArr.push(7)
+//myArr.pop(7)
+//console.log(myArr);
+
+myArr.unshift(9)
+
+console.log(myArr);
+
+
