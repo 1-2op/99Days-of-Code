fbd9fdb3c11980d0bc782b1c48de9dc10d88dc50
Afrin jahan Rimi <116110521+1-2op@users.noreply.github.com>
2024-11-27 23:30:53 +0600

    Add files via upload

---
+++ day27.js
@@ -0,0 +1,37 @@
+//break,continue
+
+//for (let index = 0; index <= 20; index++) {
+    //if(index == 5){
+        //console.log(`Detected 5`);
+        //break
+    //}
+    //console.log(`value of i is ${index}`);
+
+    
+//}
+
+//for (let index = 0; index <= 20; index++) {
+    //if(index == 5){
+        //console.log(`Detected 5`);
+        //continue
+    //}
+    //console.log(`value of i is ${index}`);
+
+    
+//}
+
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+    
+}
+
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <= 10);
