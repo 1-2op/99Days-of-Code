0e3cd78ebc3d57a6f5db2a62c10aec88c19c00ce
Afrin jahan Rimi <116110521+1-2op@users.noreply.github.com>
2024-12-04 00:00:08 +0600

    Add files via upload

---
+++ day33.js
@@ -0,0 +1,14 @@
+//const  newNums = []
+//myNums.forEach(  (num) => {
+    //if ( num > 4) {
+        //newNums.push(num)
+
+    //}
+//})
+//console.log(newNums)
+
+const myNumbers = [1, 2, 3, 4, 5]
+
+const newNums = myNumbers.map(  (num) => num + 10)
+
+ console.log(newNums);
